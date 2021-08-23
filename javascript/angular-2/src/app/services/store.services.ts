487363dff3d2fba0a1dd0b786c08b63a213e19c9
darya moroz <darya.moroz@etalongroup.com>
2021-08-23 08:28:39 +0300

    angular-2

---
+++ javascript/angular-2/src/app/services/store.services.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor() {}
+
+  set(key: string, data: any): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      console.error('Error saving to localStorage', e);
+    }
+  }
+  get(key: string) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (e) {
+      console.error('Error getting data from localStorage', e);
+      return null;
+    }
+  }
+  len() {
+    try {
+      return localStorage.length;
+    } catch (e) {
+      console.error('Error getting length from localStorage', e);
+      return null;
+    }
+  }
+  del(key: string): void {
+    try {
+      localStorage.removeItem(key);
+    } catch (e) {
+      console.error('Error saving to localStorage', e);
+    }
+  }
+  k(i: number) {
+    try {
+      return localStorage.key(i);
+    } catch (e) {
+      console.error('Error getting length from localStorage', e);
+      return null;
+    }
+  }
+}
